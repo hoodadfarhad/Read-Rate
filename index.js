5a1637ed9ff2b949ecf2d1095685550465c21429
Hoodad Farhad <hoodados@Hoodads-MacBook-Air.local>
2025-02-21 16:14:16 -0500

    .

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "books",
+  password: "dbhoodad",
+  port: 5432,
+});
+db.connect();
+
+app.listen(port, ()=>{console.log("Server is running on port "+port)})
